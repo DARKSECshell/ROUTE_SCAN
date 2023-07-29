83405b4657fd9817553fd8e2f5c77c09fd4a1ced
DARKSEC <121623691+DARKSECshell@users.noreply.github.com>
2023-07-29 13:19:46 -0300

    dark.sh

--- pivoting.sh
+++ pivoting.sh
@@ -4,7 +4,7 @@
 if [ "$1" == "" ]
 then
 	echo "USE DESSA FORMA : ./$0 <site ou o ip>"
-	echo "LEMBRANDO QUE ESTÁ FERRAMENTA SERVE PARA MONITORAR TODA ROTA NO HOST ALVO"
+	echo "LEMBRANDO QUE ESTÁ FERRAMENTA SERVE PARA MONITORAR TODA ROTA NO HOST ALVO" | touch trace.txt
 else
 for i in {1..255}; do
 (ping -c 1 -t $i $1 | grep "From" | awk -F " " '{print "ALVOS :" $2}' &);
