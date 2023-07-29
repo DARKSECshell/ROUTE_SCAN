a0da9a8c3a3968f26e4cf92e0d3d5ec414b34973
DARKSEC <121623691+DARKSECshell@users.noreply.github.com>
2023-07-29 13:26:11 -0300

    route.sh

--- pivoting.sh
+++ pivoting.sh
@@ -3,9 +3,13 @@
 
 if [ "$1" == "" ]
 then
-	echo "USE DESSA FORMA : ./$0 <site ou o ip>"
-	echo "LEMBRANDO QUE ESTÁ FERRAMENTA SERVE PARA MONITORAR TODA ROTA NO HOST ALVO" | touch trace.txt
+# crianção do arquivo .txt
+touch trace.txt
+# AJUDA
+ 	echo "USE DESSA FORMA : ./$0 <site ou o ip>"
+	echo "LEMBRANDO QUE ESTÁ FERRAMENTA SERVE PARA MONITORAR TODA ROTA NO HOST ALVO"
 else
+# ACÃO DA FERRAMENTA A TOOL RODA UM WORDLISTS NO 
 for i in {1..255}; do
 (ping -c 1 -t $i $1 | grep "From" | awk -F " " '{print "ALVOS :" $2}' &);
 done 2> /dev/null > trace.txt
